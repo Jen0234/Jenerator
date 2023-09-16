7d488e66a444c3d0939239a223c259a57236f3d2
Jen0234 <JennaLafable@gmail.com>
2023-09-16 17:01:17 -0400

    Create Jenerator.js
    add my strings

---
+++ Jenerator.js
@@ -0,0 +1,16 @@
+const upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+const lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+const number = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+const specialCase = [ '!','"', '#', '$', '%', '&','(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[',  '^', '-' , '`', '{', '|', '}', '~' ];
+const password = upperCase + lowerCase + number + specialCase ;
+console.log(password.length);
+
+//Password of Eight Charactor
+ function passwordOfEight(){
+   return 
+    
+ }
+ passwordOfEight()
+//Password of Ten Charactor
+
+//Password of Four Charactor Only Number 
