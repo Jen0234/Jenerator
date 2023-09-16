89daed54f9ccf018177da733335c24cae7fbbae8
Jen0234 <JennaLafable@gmail.com>
2023-09-16 19:15:16 -0400

    Update Jenerator.js
    Fix all the strings
    
    i made a options for password 8 or 10 length of charactor

--- Jenerator.js
+++ Jenerator.js
@@ -1,16 +1,39 @@
-const upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
-const lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-const number = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-const specialCase = [ '!','"', '#', '$', '%', '&','(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[',  '^', '-' , '`', '{', '|', '}', '~' ];
-const password = upperCase + lowerCase + number + specialCase ;
-console.log(password.length);
+const upperCase = [ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ];
+const lowerCase = [ 'abcdefghijklmnopqrstuvwxyz' ];
+const number = [ '0123456789' ];
+const specialCase = [ '!"#$%&()*+-./:;<=>?@[^-`{|}~' ];
 
 //Password of Eight Charactor
- function passwordOfEight(){
-   return 
-    
+ const password = upperCase + lowerCase + number + specialCase ;
+ function EightChar(length){
+    let result = '';
+    const passwordlength = password.length;
+    for (let i = 0; i < length; i++){
+        result += password.charAt(Math.floor(Math.random() * passwordlength));
+    }  
+     return result;  
  }
- passwordOfEight()
+ console.log(EightChar(8));
+
+
+
 //Password of Ten Charactor
+ tenPass = upperCase + lowerCase + number + specialCase ;
+ function TenChar(length){
+    let result = '';
+    const tenPasslength = tenPass.length;
+    for (let i = 0; i < length; i++){
+        result += tenPass.charAt(Math.floor(Math.random() * tenPasslength));
+    }  
+     return result;  
+ }
+ console.log(TenChar(10));
+
 
 //Password of Four Charactor Only Number 
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const shuffledArray = array.sort(() => 0.5 - Math.random());
+// console.log(shuffledArray)
+
+// declare the function 
